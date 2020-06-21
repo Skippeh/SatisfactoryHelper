b640e2dfa088f2f412f72dbbef17dd946e98579a
Skipcast <jonathanjl99@gmail.com>
2020-06-21 15:06:25 +0200

    Made SHRecipeHelper into a valid class for ue4 reflection/bp

--- Source/SatisfactoryHelper/Public/SHRecipeHelper.h
+++ Source/SatisfactoryHelper/Public/SHRecipeHelper.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "SHRecipeHelper.generated.h"
 
 class UFGRecipe;
 class UFGItemDescriptor;
@@ -8,8 +9,13 @@ class UFGItemDescriptor;
 /**
  * This class exists to be able to find recipes by various filters. We can't use FGRecipeManager because it only searches through unlocked recipes.
  */
-class SATISFACTORYHELPER_API SHRecipeHelper
+UCLASS(Abstract)
+class SATISFACTORYHELPER_API USHRecipeHelper : public UObject
 {
+	GENERATED_BODY()
+
 public:
+	/** Returns all recipes that output the given item. */
+	UFUNCTION(BlueprintCallable, BlueprintPure)
 	static TArray<TSubclassOf<UFGRecipe>> FindRecipesByProduct(UObject* WorldContextObject, TSubclassOf<UFGItemDescriptor> ItemDescriptor);
 };
